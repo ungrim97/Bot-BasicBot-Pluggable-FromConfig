38c4e4143d3d21a96a975d9ba51411588c2bb8b3
Mike Francis <ungrim97@gmail.com>
2014-03-20 13:52:02 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Bot::BasicBot::Pluggable::FromConfig - It's new $module
+
+# SYNOPSIS
+
+    use Bot::BasicBot::Pluggable::FromConfig;
+
+# DESCRIPTION
+
+Bot::BasicBot::Pluggable::FromConfig is ...
+
+# LICENSE
+
+Copyright (C) Mike Francis.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mike Francis <ungrim97@gmail.com>
---
+++ lib/Bot/BasicBot/Pluggable/FromConfig.pm
@@ -0,0 +1,39 @@
+package Bot::BasicBot::Pluggable::FromConfig;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Bot::BasicBot::Pluggable::FromConfig - It's new $module
+
+=head1 SYNOPSIS
+
+    use Bot::BasicBot::Pluggable::FromConfig;
+
+=head1 DESCRIPTION
+
+Bot::BasicBot::Pluggable::FromConfig is ...
+
+=head1 LICENSE
+
+Copyright (C) Mike Francis.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mike Francis E<lt>ungrim97@gmail.comE<gt>
+
+=cut
+
